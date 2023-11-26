f74b810acfa2ccb0f92c4527943895f0018a7c8a
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-11-26 16:22:38 -0300

    help c;

--- express/src/server.ts
+++ express/src/server.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-
 import express from "express";
 import { createUploadthingExpressHandler } from "uploadthing/express";
 import { uploadRouter } from "./uploadthing";
@@ -7,6 +6,16 @@ import cors from "cors";
 
 const app = express();
 app.use("*", cors());
+console.log("process:", process.env.UPLOADTHING_SECRET);
+console.log("id", process.env.UPLOADTHING_APP_ID);
+
+// Middleware to log incoming requests
+app.use((req, res, next) => {
+  console.log(`Incoming ${req.method} request to ${req.originalUrl}`);
+  console.log("Request headers:", req.headers);
+  console.log("Request body:", req.body);
+  next();
+});
 
 app.use(
   "/api/uploadthing",
